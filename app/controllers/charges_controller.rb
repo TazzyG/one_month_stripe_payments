2eaeccc03f6f9d81893318f554c9941b8e253f3f
Laurie Gloge <gloge@haathi-consulting.com>
2015-10-08 11:28:28 -0600

    Implemented subscription based product GROHACK2

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -2,26 +2,27 @@ class ChargesController < ApplicationController
 	
 		def create
 			
-		product = Product.find_by_sku('GROWHACK1')
+		product = Product.find_by_sku('GROHACK2')
 
 	  customer = Stripe::Customer.create(
 	    :email => params[:stripeEmail],
-	    :card  => params[:stripeToken]
+	    :card  => params[:stripeToken],
+	    :plan  => "GROHACK2"
 	  )
 
-	  charge = Stripe::Charge.create(
-	    :customer    => customer.id,
-	    :amount      => product.price_in_cents,
-	    :description => 'Growth Hacking Crash Courser',
-	    :currency    => 'usd'
-	  )
+	  # charge = Stripe::Charge.create(
+	  #   :customer    => customer.id,
+	  #   :amount      => product.price_in_cents,
+	  #   :description => product.full_description,
+	  #   :currency    => 'usd'
+	  # )
 
 	  purchase = Purchase.create(
 	  	email: params[:stripeEmail], 
 	  	card: params[:stripeToken], 
 	  	amount: product.price_in_cents, 
-	  	description: charge.description, 
-	  	currency: charge.currency, 
+	  	description: product.full_description, 
+	  	currency: 'cad', 
 	  	customer_id: customer.id,
 	  	product_id: product.id, 
 	  	uuid: SecureRandom.uuid
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
 	def home
-		@product = Product.find_by_sku("GROWHACK1")
+		@product = Product.find_by_sku("GROHACK2")
 	end
 end
